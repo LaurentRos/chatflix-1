d183ab1749576748157dbfcdce0cb28b6e72bbfd
Laurent Ros <laurentr@theodo.fr>
2017-06-15 19:59:02 +0200

    [Feature] Update Question

--- recommendation.py
+++ recommendation.py
@@ -74,7 +74,7 @@ class Recommendation:
 
     # Pose une question à l'utilisateur
     def ask_question(self, user):
-        return "Vous aimez les films ?"
+        return "J'aime les films. Vous aimez les films ?"
 
     # Calcule la similarité entre 2 utilisateurs
     @staticmethod
